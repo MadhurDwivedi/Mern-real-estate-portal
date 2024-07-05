4f5b6ca184ae81a63e3b5ffdea6b2ee461e35106
Madhur Dwivedi <137537578+MadhurDwivedi@users.noreply.github.com>
2024-07-05 20:12:17 +0530

    complete sign up page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+//import OAuth from '../components/OAuth';
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
@@ -7,7 +8,7 @@ export default function SignUp() {
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({
-      ...formData,
+      ...formData, 
       [e.target.id]: e.target.value,
     });
   };
@@ -32,12 +33,15 @@ export default function SignUp() {
       setLoading(false);
       setError(null);
       navigate('/sign-in');
-    } catch (error) {
+    } 
+    catch (error) {
       setLoading(false);
       setError(error.message);
     }
   };
-  return (<div className='p-3 max-w-lg mx-auto'>
+
+  return (
+  <div className='p-3 max-w-lg mx-auto'>
     <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
     <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
       <input
@@ -61,21 +65,21 @@ export default function SignUp() {
         id='password'
         onChange={handleChange}
       />
-
       <button
       disabled={loading}
-        className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+      className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
       >
         {loading ? 'Loading...' : 'Sign Up'}
       </button>
+      {/* <OAuth/> */}
     </form>
     <div className="flex gap-2 mt-5">
       <p>Have an account?</p>
       <Link to={"/sign-in"}>
-        <span className='text-blue-700'>Sign in</span>
+      <span className='text-blue-700'>Sign in</span>
       </Link>
     </div>
     {error && <p className='text-red-500 mt-5'>{error}</p>}
   </div>
-);
+  );
 }
